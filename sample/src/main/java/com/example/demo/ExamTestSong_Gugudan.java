f7c95bb50915baff7b3c654605f6a13bbc443434
DESKTOP-048HU3O\ECS <wodlr1310@naver.com>
2024-05-03 15:48:00 +0900

    [송재익] [

---
+++ sample/src/main/java/com/example/demo/ExamTestSong_Gugudan.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class ExamTestSong_Gugudan {
+
+	public void ExamPleSong() {
+		int a = 0;
+		int b = 2;
+		int result = a + b;
+		System.out.println(result);
+	}
+}
