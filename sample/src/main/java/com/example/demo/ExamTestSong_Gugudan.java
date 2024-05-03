b5d0ae5cf4070b5a3f9f8f076bca094f34895da6
DESKTOP-048HU3O\ECS <wodlr1310@naver.com>
2024-05-03 16:05:43 +0900

    [송재익][새롭게 올라간 파일]

--- sample/src/main/java/com/example/demo/ExamTestSong_Gugudan.java
+++ sample/src/main/java/com/example/demo/ExamTestSong_Gugudan.java
@@ -8,4 +8,21 @@ public class ExamTestSong_Gugudan {
 		int result = a + b;
 		System.out.println(result);
 	}
+	
+	public void ExamPleSong1() {
+		boolean song = false;
+		int a = (int)Math.random()*10 + 1;
+		while(song) {
+			System.out.println("주사위 게임에 오신걸 환영합니다");
+			switch (a) {
+			case 1: {
+				System.out.println("오늘의 행운의 숫자는" + a);
+				break;
+			}
+			default:
+				song = true;
+			}
+		}
+		
+	}
 }
