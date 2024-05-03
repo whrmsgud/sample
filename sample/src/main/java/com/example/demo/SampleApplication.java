17dcaeab49b0e2404e9ff5bb3e03eca9a5b5974c
DESKTOP-048HU3O\ECS <elfflqjem3366@naver.com>
2024-05-03 15:13:29 +0900

    커밋

--- sample/src/main/java/com/example/demo/SampleApplication.java
+++ sample/src/main/java/com/example/demo/SampleApplication.java
@@ -9,6 +9,7 @@ public class SampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
 		System.out.println("test2");
+		System.out.println("test3");
 	}
 
 }
